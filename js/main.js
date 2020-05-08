ed885cbee33b605bcdf2c939048de0b1d322f146
Ryan Ross <ryguyeditor@gmail.com>
2020-05-08 10:32:58 -0700

    project 1 assesment

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-console.log ('hello world')
+let input = 0
+
+
+
+//Handlers
+
+let $inputCounter = $('.solution').text($inputCounter).appendTo('.input')
+
+const display = () => {
+    $('.solution').children().remove()
+    $('<h1>').text(`${counter}`).appendTo('.input')
+}
+
+const checkInput = () => {
+    let $input = parseInt($('.input').val())
+        //console.log($input)
+        return $input
+}
+
+
+
+const minus = $('.minus').on('click', ()=>{
+    let subtract = checkInput()
+    if (subtract >= 0){
+        inputCounter -= subtract
+        displayUpdate()
+    }
+    if (inputCounter < 0){
+        $('.solution').addClass('red')
+    }
+})
+const plus = $('.plus').on('click', ()=>{
+    let add = checkInput()
+    if (add >= 0){
+        inputCounter += Add
+        displayUpdate()
+    }
+    if (inputCounter < 0){
+    $('.solution').addClass('red')
+    }
+})
